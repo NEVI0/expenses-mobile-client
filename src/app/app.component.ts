df54b256ad423759d4bb4dbc15520444d13e1bee
Nevio <neviocostamagagnin@gmail.com>
2020-02-23 11:45:14 -0300

    Creating the token validation to do the authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Storage } from '@ionic/storage';
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { environment } from '../environments/environment';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -16,12 +18,17 @@ import { environment } from '../environments/environment';
 export class AppComponent implements OnInit {
 
     private readonly StorageTheme = environment.StorageTheme;
+    private readonly AppUserData = environment.StorageUserData;
+    private readonly ApplicationUrl = environment.ApplicationUrl;
 
     constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
         private statusBar: StatusBar,
-        private storage: Storage
+        private storage: Storage,
+        private router: Router,
+        private authService: AuthService,
+        private alertCtrl: AlertController,
     ) {
         this.initializeApp();
     }
@@ -34,6 +41,34 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.storage.get(this.AppUserData).then(value => {
+            this.authService.validateToken(JSON.parse(value)).subscribe(
+                async (resp: any) => {
+
+                    if (resp.valid !== true) {
+                        if (window.location.href !== `${this.ApplicationUrl}/auth`) {
+
+                            const alertRef = await this.alertCtrl.create({
+                                header: 'Alerta!',
+                                message: 'Ops! Você <b>precisa fazer o login</b> para poder usar o app',
+                                buttons: [{
+                                    text: 'Fazer Login',
+                                    handler: () => {
+                                        this.router.navigate(['/auth']);
+                                    }
+                                }]
+                            });
+                            alertRef.present();
+                            alertRef.onWillDismiss().then(() => {
+                                this.router.navigate(['/auth']);
+                            });
+
+                        }
+                    }
+
+                }
+            );
+        });
         this.storage.get(this.StorageTheme).then(value => {
             if (value == 'light') {
                 document.body.classList.remove('dark');
