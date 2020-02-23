8ff96c141ad5eb61ce7c5045571649ee71bf68f9
Nevio <neviocostamagagnin@gmail.com>
2020-02-23 11:42:38 -0300

    Adding the new config vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
     production: true,
-    StorageTheme: '_app_storage_theme'
+    StorageTheme: '_app_storage_theme',
+    StorageUserData: '_app_storage_user_data',
+    ApplicationUrl: 'http://localhost:8100',
+    OpenedApiUrl: 'https://expenses-server-api.herokuapp.com/oapi',
+    BlockedApiUrl: 'https://expenses-server-api.herokuapp.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,11 @@
 
 export const environment = {
     production: false,
-    StorageTheme: '_app_storage_theme'
+    StorageTheme: '_app_storage_theme',
+    StorageUserData: '_app_storage_user_data',
+    ApplicationUrl: 'http://localhost:8100',
+    OpenedApiUrl: 'https://expenses-server-api.herokuapp.com/oapi',
+    BlockedApiUrl: 'https://expenses-server-api.herokuapp.com/api'
 };
 
 /*
