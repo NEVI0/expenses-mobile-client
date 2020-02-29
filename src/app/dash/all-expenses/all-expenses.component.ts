918aa957be962701f7dfa8ed9eabb4b5f08726fb
Nevio <neviocostamagagnin@gmail.com>
2020-02-29 13:05:46 -0300

    Adding the list of all expenses and the search box

--- src/app/dash/all-expenses/all-expenses.component.ts
+++ src/app/dash/all-expenses/all-expenses.component.ts
@@ -1,14 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { Storage } from '@ionic/storage';
+
+import { Observable } from 'rxjs';
+import { map, debounceTime, switchMap, distinctUntilChanged } from 'rxjs/operators';
+
+import { DashService } from '../dash.service';
+import { Expense } from '../../interfaces/Expense';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-all-expenses',
   templateUrl: './all-expenses.component.html',
   styleUrls: ['./all-expenses.component.scss'],
 })
+
 export class AllExpensesComponent implements OnInit {
 
-  constructor() { }
+    private readonly AppUserData = environment.StorageUserData;
 
-  ngOnInit() {}
+    public expenses$: Observable<Expense[] | any>;
+    public searchResults$: Observable<Expense[] | any>;
+
+    public tag = new FormControl();
+    public userToken: string;
+
+    constructor(
+        private dashService: DashService,
+        private storage: Storage
+    ) {}
+
+    ngOnInit() {
+        this.onStart()
+    }
+
+    ionViewWillEnter() {
+        this.onStart()
+    }
+
+    onStart() {
+        this.storage.get(this.AppUserData).then(value => {
+            const user = JSON.parse(value);
+
+            this.userToken = user.token;
+            this.expenses$ = this.dashService.getAllExpenses(user._id, user.token);
+            this.searchResults$ = this.tag.valueChanges.pipe(
+                map(result => result.trim()),
+                debounceTime(250),
+                distinctUntilChanged(),
+                switchMap(result => this.dashService.search(user._id, result, user.token))
+            );
+        });
+    }
+
+    onRefresh(event) {
+        setTimeout(() => {
+            this.onStart();
+            event.target.complete();
+        }, 1500);
+    }
 
 }
